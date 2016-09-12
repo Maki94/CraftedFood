fba7a3d9b53318b6f5898e70399643c46b4fecae
Plavusa <masadordevic@gmail.com>
2016-09-12 21:05:35 +0200

    radi2

---
+++ CrarftedFood/CrarftedFood.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Data;
+
+namespace CrarftedFood.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Data.Entities.Emploies.AddEmployee("Masa Djordjevic", "masa@gmail.com", "0648096042", "masa");
+
+        }
+    }
+}
