50c67a5c79b3813b87a1a32709c21a2d59999761
nikolcar <mitic.nikolca94@gmail.com>
2016-09-13 12:06:49 +0200

    Slanje mail-a prilikom kreiranja

--- CrarftedFood/CrarftedFood/Controllers/EmployeeController.cs
+++ CrarftedFood/CrarftedFood/Controllers/EmployeeController.cs
@@ -5,6 +5,9 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
 using CrarftedFood.Models;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Net;
 
 namespace CrarftedFood.Controllers
 {
@@ -22,15 +25,45 @@ namespace CrarftedFood.Controllers
         }
 
         [HttpPost]
-        public ActionResult AddEmployee(AddEmployeeViewModel model)
+        public async Task<ActionResult> AddEmployee(AddEmployeeViewModel model)
         {
             string pass = Membership.GeneratePassword(7, 0);
             string hashedPass = Data.Entities.HashPassword.SaltedHashPassword(pass, model.Email);
             Data.Entities.Employees.AddEmployee(model.Name, model.Email, hashedPass, model.Role);
 
-            //TODO posalji mail radniku da je kreiran nalog i koji mu je pass
-
+            await SendEmail(model.Email, model.Name, pass, model.Role);
             return View();
+        }
+
+        public async Task SendEmail(string email, string name, string password, Data.Entities.Roles role, byte[] pdf = null)
+        {
+            //MemoryStream stream = new MemoryStream(pdf);
+            string admin = "vatreneskoljke@gmail.com";
+            string adminpass = "seashellsonfire";
+
+            var body = "<p>Poštovani {0},</p> <p> Upravo ste dodati u bazu Crafted Food radi lakšeg naručivanja hrane kao <strong>{1}</strong>, Vaši podaci za logovanje su: <br> username: {2} <br>  password: <font color=blue>{3}</p><p>Pozdrav</p>"; 
+            var message = new MailMessage();
+            message.To.Add(new MailAddress(email));
+            message.From = new MailAddress(admin);
+            message.Subject = "Welcome";
+            message.Body = string.Format(body, name, role, email, password);
+            message.IsBodyHtml = true;
+            //message.Attachments.Add(new Attachment(stream, "Request.pdf", System.Net.Mime.MediaTypeNames.Application.Pdf));
+
+
+            using (var smtp = new SmtpClient())
+            {
+                var credential = new NetworkCredential
+                {
+                    UserName = admin,
+                    Password = adminpass
+                };
+                smtp.Credentials = credential;
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.EnableSsl = true;
+                await smtp.SendMailAsync(message);
+            }
         }
     }
 }
