4ed741774403146710ed8d454f9ad9f22787522a
DESKTOP-3H8FG3A\maki <markomihajlovicfm@gmail.com>
2016-09-13 10:46:37 +0200

    zavrsen edit meal

--- CrarftedFood/Data/Entities/Meals.cs
+++ CrarftedFood/Data/Entities/Meals.cs
@@ -1,8 +1,12 @@
-﻿namespace Data.Entities
+﻿using System;
+using System.Linq;
+
+namespace Data.Entities
 {
     public static class Meals
     {
-        public static void AddMeal(string title, string descirption, byte[] image, float price, float quantity, Units unit, Categories category)
+        public static void AddMeal(string title, string descirption, byte[] image, float price, float quantity,
+            Units unit, Categories category)
         {
             using (var dc = new DataClassesDataContext())
             {
@@ -31,10 +35,30 @@
             }
         }
 
-        public static void EditMeal(Meal meal)
+        public static void EditMeal(int mealId, string title = null, string descirption = null, byte[] image = null,
+            float price = -1, float quantity = -1, Units unit = 0, Categories category = 0)
         {
-            // TODO:
+            using (var dc = new DataClassesDataContext())
+            {
+                try
+                {
+                    var meal = dc.Meals.First(x => x.MealId == mealId);
 
+                    if (title != null) meal.Title = title;
+                    if (descirption != null) meal.Description = descirption;
+                    if (image != null) meal.Image = image;
+                    if (price != -1) meal.Price = price;
+                    if (quantity != -1) meal.Quantity = quantity;
+                    if (unit != 0) meal.UnitId = (int) unit;
+                    if (category != 0) meal.CategoryId = (int) category;
+
+                    dc.SubmitChanges();
+                }
+                catch (Exception e)
+                {
+                    throw;
+                }
+            }
         }
     }
 }
