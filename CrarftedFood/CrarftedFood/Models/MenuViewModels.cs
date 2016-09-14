b67cae4be7a47e9a8489577cf35f58c6f69c39e5
nikolcar <mitic.nikolca94@gmail.com>
2016-09-14 11:16:29 +0200

    Enum sredjeno

--- CrarftedFood/CrarftedFood/Models/MenuViewModels.cs
+++ CrarftedFood/CrarftedFood/Models/MenuViewModels.cs
@@ -18,11 +18,11 @@ namespace CrarftedFood.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public Data.Entities.Categories Category { get; set; }
+        public Data.Enums.Categories Category { get; set; }
         public float Quantity { get; set; }
         public byte[] Image { get; set; }
         public float Price { get; set; }
-        public Data.Entities.Units Unit { get; set; }
+        public Data.Enums.Units Unit { get; set; }
 
         public static MealViewModel Load(int mealId)
         {
@@ -34,11 +34,11 @@ namespace CrarftedFood.Models
                 Id = meal.MealId,
                 Title = meal.Title,
                 Description = meal.Description,
-                Category = (Data.Entities.Categories) meal.CategoryId,
+                Category = (Data.Enums.Categories) meal.CategoryId,
                 Quantity = meal.Quantity,
                 Image = meal.Image.ToArray(),
                 Price = meal.Price,
-                Unit = (Data.Entities.Units) meal.UnitId
+                Unit = (Data.Enums.Units) meal.UnitId
             };
         }
 
