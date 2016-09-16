fa59196fa08025570f652f4d3f66bee714832d3e
nikolcar <mitic.nikolca94@gmail.com>
2016-09-16 11:24:05 +0200

    PDF Rotativa in, itextsharper out

--- CrarftedFood/CrarftedFood/Controllers/ReportsController.cs
+++ CrarftedFood/CrarftedFood/Controllers/ReportsController.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 using CrarftedFood.Models;
 using Data.DTOs;
 using Data.Entities;
+using Rotativa.Options;
+using Size = Rotativa.Options.Size;
 
 namespace CrarftedFood.Controllers
 {
@@ -36,6 +41,8 @@ namespace CrarftedFood.Controllers
             //}
 
             // NOTE: temporary 
+
+
             startTime = new DateTime(2000, 1,1);
             endTime = new DateTime(2017, 1,1);
 
@@ -43,8 +50,29 @@ namespace CrarftedFood.Controllers
             {
                 Orders = Reports.GetInvoiceReport(startTime, endTime)
             };
-
+            
+            var PDF = new Rotativa.ViewAsPdf("Invoice", order)
+            {
+                PageSize = Size.A4
+            };
+            byte[] bytePDF = PDF.BuildPdf(this.ControllerContext);
+            var fileStream = new FileStream(System.Web.HttpContext.Current.Server.MapPath("~") + "/RESOURCES/" + DateTime.Now.ToLongDateString() + ".pdf", FileMode.Create, FileAccess.Write);
+            fileStream.Write(bytePDF, 0, bytePDF.Length);
+            fileStream.Close();
             return View(order);
+        }
+
+        private string ViewToString(string viewName, object model)
+        {
+            ViewData.Model = model;
+            using (var sw = new System.IO.StringWriter())
+            {
+                var viewResult = ViewEngines.Engines.FindPartialView(ControllerContext, viewName);
+                var viewContext = new ViewContext(ControllerContext, viewResult.View, ViewData, TempData, sw);
+                viewResult.View.Render(viewContext, sw);
+                viewResult.ViewEngine.ReleaseView(ControllerContext, viewResult.View);
+                return sw.GetStringBuilder().ToString();
+            }
         }
 
 
