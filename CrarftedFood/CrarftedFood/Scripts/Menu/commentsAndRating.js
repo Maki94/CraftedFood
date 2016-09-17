52fddd8618f2fdd741d4d851c9f5314fb7ea4294
Plavusa <masadordevic@gmail.com>
2016-09-17 11:51:28 +0200

    javascript rasclanjen

---
+++ CrarftedFood/CrarftedFood/Scripts/Menu/commentsAndRating.js
@@ -0,0 +1,110 @@
+﻿//rating
+$('.rating-list > li').on('click', function (e) {
+    sendData = {
+        mealId: this.parentElement.parentElement.parentElement.parentElement.getElementsByClassName('meal-id')[0].innerHTML,
+        rating: parseInt(this.innerHTML.charAt(0))
+    };
+
+    $.ajax({
+        url: url.rateMeal,
+        method: "POST",
+        data: sendData,
+        success: function (result) {
+            var inner = document.getElementById(sendData.mealId).getElementsByClassName('rating')[0].innerHTML;
+            document.getElementById(sendData.mealId).getElementsByClassName('rating')[0].innerHTML = result.newRating + " " + inner.substr(inner.indexOf("<"));
+            var snackbarContainer = document.querySelector('#demo-toast-example');
+            var data = { message: 'Rate submited' };
+            snackbarContainer.MaterialSnackbar.showSnackbar(data);
+        }
+    });
+});
+
+// postavljanje komentara na enter
+var ENTER_CODE = 13;
+function eventFire(el, etype) {
+    if (el.fireEvent) {
+        el.fireEvent('on' + etype);
+    } else {
+        var evObj = document.createEvent('Events');
+        evObj.initEvent(etype, true, false);
+        el.dispatchEvent(evObj);
+    }
+}
+
+$('#menu-comment').bind('keypress', function (event) {
+    var x = event.keyCode;
+    if (x === ENTER_CODE) {
+        eventFire(document.getElementById('menu-comment-share'), 'click');
+    }
+});
+// END
+
+function formatDate(date) {
+    return date.getDate() + "." + (date.getMonth() + 1) + "." + date.getFullYear();
+}
+
+//show comments
+$('.comments_button').on('click', function (e) {
+    self = this;
+    mealId = this.parentElement.parentElement.parentElement.parentElement.getElementsByClassName('meal-id')[0].innerHTML;
+
+    if (this.parentElement.parentElement.getElementsByClassName('comments')[0].classList.contains('hide')) {
+        //get comments
+        $.ajax({
+            url: url.getComments,
+            method: "POST",
+            data: { mealId: mealId },
+            success: function (result) {
+                if (result.success === true) {
+                    var container = self.parentElement.parentElement.querySelector('.comments :first-child');
+                    container.innerHTML = "";
+                    for (var i = 0; i < result.comments.length; i++) {
+                        container.innerHTML += '<div><h6>' + result.comments[i].CommenterName + '</h6><span class="date">' + formatDate(new Date("" + result.comments[i].Date)) + '</span><p>' + result.comments[i].Comment + '</p></div>';
+                    }
+                }
+            }
+        });
+    }
+
+    ///hide immage
+    this.parentElement.parentElement.parentElement.getElementsByClassName('card__image')[0].classList.toggle('shrink');
+
+    //toggle content
+    this.parentElement.parentElement.getElementsByClassName('comments')[0].classList.toggle('hide');
+    this.parentElement.parentElement.getElementsByClassName('card__article')[0].classList.toggle('hide');
+
+    //toggle icon
+    $(this).children('i').toggleClass('fa-reply fa-comments');
+});
+
+//add comment
+$('.add-comment-wrapper button').on('click', function (e) {
+    input = this.parentElement.getElementsByTagName('textarea')[0];
+    sendData = {
+        mealId: this.parentElement.parentElement.parentElement.parentElement.getElementsByClassName('meal-id')[0].innerHTML,
+        comment: this.parentElement.getElementsByTagName('textarea')[0].value
+    }
+
+    if (sendData.comment == null || sendData.comment.length < 1) {
+        //prikazi poruku
+        var snackbarContainer = document.querySelector('#demo-toast-example');
+        var data = { message: 'No comment to submit' };
+        snackbarContainer.MaterialSnackbar.showSnackbar(data);
+
+
+    } else {
+        //posalji komentar
+        $.ajax({
+            url: url.commentMeal,
+            method: "POST",
+            data: sendData,
+            success: function (result) {
+                var now = new Date();
+                document.querySelector('*[data-id="' + sendData.mealId + '"]').parentElement.querySelector('.comments :first-child').innerHTML +=
+                    '<div><h6>@UserSession.GetUser().Name</h6><span class="date">' + formatDate(now) + '</span><p>' + sendData.comment + '</p></div>';
+            }
+        });
+        //obrisi text polje
+        input.value = "";
+    }
+});
---
+++ CrarftedFood/CrarftedFood/Scripts/Menu/ordering.js
@@ -0,0 +1,100 @@
+﻿$(document).ready(function ($) {
+
+    //add days options
+    var days = ["pon", "uto", "sre", "cet", "pet"];
+    var today = new Date().getDay() - 1;
+    for (var i = 0; today + i < 5 || i < 2; i++) {
+        $('.card__social').append('<a class="share-icon order-btn" href="#" data-toggle="modal" data-target="#order-dialog" ' + 'attr-day="' + ((today + i) % 5) + '">' + days[(today + i) % 5] + '</span></a>');
+    }
+    $('.card__social').append('<a class="share-icon order-btn" href="#" data-toggle="modal" data-target="#order-dialog" >' + '<i class="fa fa-calendar" aria-hidden="true"></i>' + '</span></a>');
+
+});
+
+//show ordering options
+$('.card__share > a').on('click', function (e) {
+    e.preventDefault() // prevent default action - hash doesn't appear in url
+    $(this).parent().find('div').toggleClass('card__social--active');
+    $(this).toggleClass('share-expanded');
+});
+
+
+//open order dialog
+$('.order-btn').on('click', function (e) {
+    var body = "";
+    var mealId = this.parentElement.parentElement.parentElement.getElementsByClassName('meal-id')[0].innerHTML;
+    var fullDate = new Date();
+    if (this.attributes['attr-day']) {
+        var date = this.attributes['attr-day'].value - today;
+        if (date < 0)
+            date = date + 7;
+        fullDate.setDate(new Date().getDate() + date);
+    }
+
+    var dateString = fullDate.getDate() + "/" + (fullDate.getMonth() + 1) + "/" + fullDate.getFullYear();
+
+    body += '<div class="form-horizontal">';
+
+    body += '<div class="form-group"> \
+                        <label class="control-label col-md-3" for="quantity">Quantity</label> \
+                        <div class="col-md-9"> \
+                           <input class="quantity" type="number" id="quantity" value="1"/> \
+                        </div> \
+                    </div>';
+
+    body += '<div class="form-group"> \
+                        <label class="control-label col-md-3" for="Date">Choose date</label> \
+                        <div class="col-md-9"> \
+                           <input class="date" size="16" type="text" value="' + dateString + '"> \
+                           <span class="add-on"><i class="icon-th"></i></span></div> \
+                        </div>';
+
+
+    body += '<div class="form-group"> \
+                        <label class="control-label col-md-3" for="add-note">Add note</label> \
+                        <div class="col-md-9"> \
+                           <textarea type="text" id="add-note"></textarea> \
+                        </div> \
+                    </div>';
+    body += '</div>';
+
+    body += '<span class="meal-id" hidden data-id="' + mealId + '"></span>';
+
+    var dialog = document.getElementById('order-dialog');
+    dialog.querySelector('.modal-body').innerHTML = body;
+    $('.modal-body input.date').datepicker({
+        minDate: new Date(),
+        autosize: true,
+        firstDay: 1,
+        prevText: "",
+    });
+});
+
+//send order
+$('.order-confirm').on('click', function (e) {
+    self = this;
+    mealId = this.parentElement.parentElement.getElementsByClassName('meal-id')[0].attributes['data-id'].value;
+    note = this.parentElement.parentElement.getElementsByTagName('textarea')[0].value;
+    date = this.parentElement.parentElement.getElementsByClassName('date')[0].value;
+    quantity = this.parentElement.parentElement.getElementsByClassName('quantity')[0].value;
+
+    $.ajax({
+        url: url.order,
+        method: "POST",
+        data: {
+            mealId: mealId,
+            dateToDeliver: date,
+            note: note,
+            quantity: quantity
+        },
+        success: function (result) {
+
+            var snackbarContainer = document.querySelector('#demo-toast-example');
+            if (result.success === true) {
+                showSnackbar('Order complete');
+            } else {
+                showSnackbar('Error');
+            }
+        }
+    });
+
+});
